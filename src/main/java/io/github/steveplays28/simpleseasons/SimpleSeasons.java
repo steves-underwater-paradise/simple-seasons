a0f42cdfecd11711dd64444ff37a3e10baf3e6e9
Steveplays28 <djspaargaren@outlook.com>
2024-05-31 18:44:48 +0200

    refactor: Move command registration into `CommandRegistry`

--- src/main/java/io/github/steveplays28/simpleseasons/SimpleSeasons.java
+++ src/main/java/io/github/steveplays28/simpleseasons/SimpleSeasons.java
@@ -1,24 +1,18 @@
 package io.github.steveplays28.simpleseasons;
 
-import com.mojang.brigadier.builder.LiteralArgumentBuilder;
-import io.github.steveplays28.simpleseasons.command.season.SetSeasonCommand;
+import io.github.steveplays28.simpleseasons.command.CommandRegistry;
 import io.github.steveplays28.simpleseasons.server.state.ServerSeasonTracker;
 import io.github.steveplays28.simpleseasons.state.SeasonTracker;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
-import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.util.List;
 
 public class SimpleSeasons implements ModInitializer {
 	public static final String MOD_ID = "simple-seasons";
 	public static final String MOD_NAMESPACE = "simple_seasons";
 	public static final String MOD_NAME = "Simple Seasons";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_NAME);
-	public static final List<LiteralArgumentBuilder<ServerCommandSource>> COMMANDS = List.of(SetSeasonCommand.register());
 	public static final Identifier SEASON_PACKET_CHANNEL = new Identifier(MOD_NAMESPACE);
 	public static final SeasonTracker SEASON_TRACKER = new ServerSeasonTracker();
 
@@ -26,17 +20,6 @@ public class SimpleSeasons implements ModInitializer {
 	public void onInitialize() {
 		LOGGER.info("Loading {}.", MOD_NAME);
 
-		registerCommands();
-	}
-
-	/**
-	 * Registers serverside commands.
-	 */
-	private void registerCommands() {
-		CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
-			for (var command : COMMANDS) {
-				dispatcher.register(command);
-			}
-		});
+		CommandRegistry.registerCommands();
 	}
 }
---
+++ src/main/java/io/github/steveplays28/simpleseasons/command/CommandRegistry.java
@@ -0,0 +1,29 @@
+package io.github.steveplays28.simpleseasons.command;
+
+import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import io.github.steveplays28.simpleseasons.command.season.SetSeasonCommand;
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.minecraft.server.command.ServerCommandSource;
+
+import java.util.List;
+
+public class CommandRegistry {
+	/**
+	 * A {@link List} of serverside commands that should be registered in the {@link CommandRegistrationCallback} event.
+	 *
+	 * @see CommandRegistry#registerCommands
+	 * @see CommandRegistrationCallback
+	 */
+	public static final List<LiteralArgumentBuilder<ServerCommandSource>> COMMANDS = List.of(SetSeasonCommand.register());
+
+	/**
+	 * Registers serverside commands.
+	 */
+	public static void registerCommands() {
+		CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
+			for (var command : COMMANDS) {
+				dispatcher.register(command);
+			}
+		});
+	}
+}
