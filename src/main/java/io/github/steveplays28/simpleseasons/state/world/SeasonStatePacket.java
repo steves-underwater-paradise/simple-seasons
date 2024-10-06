c637a9413dc0723bf4bec23dd300465b9f6e2216
Steveplays28 <djspaargaren@outlook.com>
2024-10-06 14:34:35 +0200

    refactor: Add a nullability annotation and replace field access with getters

--- src/main/java/io/github/steveplays28/simpleseasons/state/world/SeasonStatePacket.java
+++ src/main/java/io/github/steveplays28/simpleseasons/state/world/SeasonStatePacket.java
@@ -11,18 +11,18 @@ public record SeasonStatePacket(int season, float seasonProgress) implements Fab
 	public static final @NotNull PacketType<SeasonStatePacket> PACKET_TYPE = PacketType.create(
 			new Identifier(SimpleSeasons.MOD_ID, "season_state"), SeasonStatePacket::new);
 
-	public SeasonStatePacket(PacketByteBuf buf) {
+	public SeasonStatePacket(@NotNull PacketByteBuf buf) {
 		this(buf.readInt(), buf.readFloat());
-	}
-
-	@Override
-	public void write(@NotNull PacketByteBuf buf) {
-		buf.writeInt(season);
-		buf.writeFloat(seasonProgress);
 	}
 
 	@Override
 	public @NotNull PacketType<?> getType() {
 		return PACKET_TYPE;
+	}
+
+	@Override
+	public void write(@NotNull PacketByteBuf buf) {
+		buf.writeInt(season());
+		buf.writeFloat(seasonProgress());
 	}
 }
