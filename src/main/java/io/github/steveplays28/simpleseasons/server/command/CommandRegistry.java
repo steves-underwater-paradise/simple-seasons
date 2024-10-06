de89a7612488a2c4d6787ebf8340e1bd2b7595da
ix0rai <ix0rai64@gmail.com>
2024-10-06 05:46:00 -0500

    Use an enum argument type for the `season set` command (#4)

--- src/main/java/io/github/steveplays28/simpleseasons/server/command/CommandRegistry.java
+++ src/main/java/io/github/steveplays28/simpleseasons/server/command/CommandRegistry.java
@@ -1,10 +1,15 @@
 package io.github.steveplays28.simpleseasons.server.command;
 
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import io.github.steveplays28.simpleseasons.SimpleSeasons;
 import io.github.steveplays28.simpleseasons.server.command.config.ReloadConfigCommand;
+import io.github.steveplays28.simpleseasons.server.command.season.SeasonArgumentType;
 import io.github.steveplays28.simpleseasons.server.command.season.SetSeasonCommand;
+import net.fabricmc.fabric.api.command.v2.ArgumentTypeRegistry;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.minecraft.command.argument.serialize.ConstantArgumentSerializer;
 import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.util.Identifier;
 
 import java.util.List;
 
@@ -24,6 +29,7 @@ public class CommandRegistry {
 	 * Registers serverside commands.
 	 */
 	public static void registerCommands() {
+		ArgumentTypeRegistry.registerArgumentType(Identifier.of(SimpleSeasons.MOD_ID, "season"), SeasonArgumentType.class, ConstantArgumentSerializer.of(SeasonArgumentType::season));
 		CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
 			for (var command : COMMANDS) {
 				dispatcher.register(command);
--- src/main/java/io/github/steveplays28/simpleseasons/server/command/season/SetSeasonCommand.java
+++ src/main/java/io/github/steveplays28/simpleseasons/server/command/season/SetSeasonCommand.java
@@ -1,17 +1,13 @@
 package io.github.steveplays28.simpleseasons.server.command.season;
 
-import com.mojang.brigadier.arguments.IntegerArgumentType;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import com.mojang.brigadier.context.CommandContext;
 import io.github.steveplays28.simpleseasons.api.SimpleSeasonsApi;
-import io.github.steveplays28.simpleseasons.state.world.SeasonTracker;
 import me.lucko.fabric.api.permissions.v0.Permissions;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.text.Text;
 import org.jetbrains.annotations.NotNull;
-
-import java.util.Locale;
 
 import static io.github.steveplays28.simpleseasons.SimpleSeasons.MOD_NAMESPACE;
 import static io.github.steveplays28.simpleseasons.server.command.season.SeasonCommandCategory.SEASON_COMMAND_CATEGORY;
@@ -24,7 +20,7 @@ public class SetSeasonCommand {
 
 	public static @NotNull LiteralArgumentBuilder<ServerCommandSource> register() {
 		return CommandManager.literal(MOD_NAMESPACE).then(CommandManager.literal(SEASON_COMMAND_CATEGORY).then(
-				CommandManager.literal(NAME).then(CommandManager.argument(SEASON_COMMAND_CATEGORY, IntegerArgumentType.integer()).executes(
+				CommandManager.literal(NAME).then(CommandManager.argument(SEASON_COMMAND_CATEGORY, SeasonArgumentType.season()).executes(
 						ctx -> execute(ctx, ctx.getSource())).requires(
 						(ctx) -> Permissions.check(
 								ctx,
@@ -34,28 +30,23 @@ public class SetSeasonCommand {
 	}
 
 	public static int execute(@NotNull CommandContext<ServerCommandSource> commandContext, @NotNull ServerCommandSource source) {
-		var seasonIdArgument = IntegerArgumentType.getInteger(commandContext, "season");
-		if (seasonIdArgument > SeasonTracker.Seasons.WINTER.ordinal() || seasonIdArgument < SeasonTracker.Seasons.SPRING.ordinal()) {
-			source.sendError(
-					Text.literal(String.format("Season argument can only be between 0-3 (inclusive), given: %s.", seasonIdArgument)));
-			return 1;
-		}
+		var seasonArgument = SeasonArgumentType.getSeason(commandContext, "season");
 
 		var serverWorld = source.getWorld();
-		var seasonArgument = SeasonTracker.Seasons.of(seasonIdArgument);
 		var currentSeason = SimpleSeasonsApi.getSeason(serverWorld);
-		if (seasonIdArgument == currentSeason.getId()) {
+		if (seasonArgument.getId() == currentSeason.getId()) {
 			source.sendError(Text.literal(
-					String.format("Season argument (%s) is the same as the current season (%s).", seasonIdArgument, currentSeason)));
+					String.format("Season argument (%s) is the same as the current season (%s).", seasonArgument.asString(), currentSeason.asString())));
 			return 1;
 		}
 
 		SimpleSeasonsApi.setSeason(serverWorld, seasonArgument);
 		source.sendMessage(Text.literal(
 				String.format(
-						"Set season to %s (%s).", seasonIdArgument,
-						SeasonTracker.Seasons.getName(seasonIdArgument).toLowerCase(Locale.ROOT)
-				)));
+						"Set season to %s.", seasonArgument.asString()
+				)
+		));
+
 		return 0;
 	}
 }
